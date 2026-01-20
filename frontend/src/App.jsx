14eb38bf295256fd2c063caa18dd2049fbb9e91e
sayan-acharyya <acharyyas735@gmail.com>
2026-01-21 01:00:05 +0530

    on going

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react' 
+import React from 'react'
+import { ThemeProvider } from './components/theme-provider';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import ProjectView from './pages/ProjectView';
+import Footer from './pages/Footer';
+import { ModeToggle } from './components/mode-toggle';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-     Hello
-    </div>
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* <ModeToggle /> */}
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/project/:id' element={<ProjectView />} />
+          </Routes>
+          <Footer />
+           <ToastContainer position="bottom-right" theme="dark" />
+        </Router>
+      </ThemeProvider>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <article className='px-5 mt-10 sm:mt-14 md:mt-16 lg:mt-24 xl:mt-32 sm:mx-auto w-full max-w-[1050px] flex flex-col gap-14 '>
+           
+        </article>
+    )
+}
+
+export default Home
