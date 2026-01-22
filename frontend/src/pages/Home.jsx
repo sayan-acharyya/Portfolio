32d17af1c4ef56be8955fd19ba0d2aa929a7b912
sayan-acharyya <acharyyas735@gmail.com>
2026-01-22 16:26:46 +0530

    componets are created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Hero from './sub-components/Hero'
+import Timeline from './sub-components/Timeline'
+import About from './sub-components/About'
+import Skills from './sub-components/Skills'
+import Portfolio from './sub-components/Portfolio'
+import MyApps from './sub-components/MyApps'
+import Contact from './sub-components/Contact'
 
 const Home = () => {
     return (
         <article className='px-5 mt-10 sm:mt-14 md:mt-16 lg:mt-24 xl:mt-32 sm:mx-auto w-full max-w-[1050px] flex flex-col gap-14 '>
-           
+            <Hero />
+            <Timeline />
+            <About />
+            <Skills />
+            <Portfolio />
+            <MyApps />
+            <Contact />
         </article>
     )
 }
